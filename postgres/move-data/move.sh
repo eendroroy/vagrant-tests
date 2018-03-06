deeebce3c4d217755d8a08e74b4314f53bbcb665
indrajit <eendroroy@gmail.com>
2018-03-06 18:36:07 +0600

    update psql move data script

--- postgres/move-data/move.sh
+++ postgres/move-data/move.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+NEW_DATA_DIR='/var/database/data/'
+
 sudo service postgresql-10 stop
 
-echo "data_directory = '/var/db/data/'" | sudo tee /var/lib/pgsql/10/data/postgresql.conf
+echo "data_directory = '${NEW_DATA_DIR}'" | sudo tee /var/lib/pgsql/10/data/postgresql.conf
 
-sudo mkdir -p /var/database/data/
-sudo rsync -av /var/lib/pgsql/10/data/ /var/database/data/
+sudo mkdir -p ${NEW_DATA_DIR}
+sudo rsync -av /var/lib/pgsql/10/data/ ${NEW_DATA_DIR}
 sudo chown postgres:postgres /var/database/ -R
 
 sudo service postgresql-10 start
